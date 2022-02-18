df62789ad2c5a529b8a24a3a5c9b7dcd867eda91
marciocamello <marciocamello@oulook.com>
2022-02-18 12:28:37 +0000

    Start react native by rocketseat ignite

---
+++ src/pages/Home.js
@@ -0,0 +1,75 @@
+import React from 'react'
+
+import {
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TextInput,
+    Platform,
+    TouchableOpacity
+} from 'react-native'
+
+export function Home() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Welcome Marcio</Text>
+
+            <TextInput
+                style={styles.input}
+                placeholder="New skill"
+                placeholderTextColor="#555"
+            />
+
+            <TouchableOpacity
+                style={styles.button}
+                activeOpacity={0.7}
+            >
+                <Text style={styles.buttonText}>Add</Text>
+            </TouchableOpacity>
+
+            <Text
+                style={[styles.title, {
+                    marginTop: 50
+                }]}
+            >
+                My Skills
+            </Text>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#121015',
+        paddingHorizontal: 20,
+        paddingHorizontal: 30,
+        paddingVertical: 70
+    },
+    title: {
+        color: "#fff",
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    input: {
+        backgroundColor: '#1f1e25',
+        color: '#fff',
+        fontSize: 18,
+        padding: Platform.OS === 'ios' ? 15 : 10,
+        marginTop: 30,
+        borderRadius: 7
+    },
+    button: {
+        backgroundColor: '#a370f7',
+        padding: 15,
+        borderRadius: 7,
+        alignItems: 'center',
+        marginTop: 20
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 17,
+        fontWeight: 'bold',
+    }
+
+});
